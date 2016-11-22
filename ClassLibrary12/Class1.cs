76237becd2e324b47565a880d9a4475388fed883
David Fowler <davidfowl@gmail.com>
2016-11-21 22:54:45 -0800

    Initial commit

---
+++ ClassLibrary12/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ClassLibrary12
+{
+    public class Class1
+    {
+        public static string Foo()
+        {
+            return JsonConvert.SerializeObject(new { A = 1 });
+        }
+    }
+}
